97502604a9e896fa46ffd22d6ac30428863ec33a
david <davidcoelho89@gmail.com>
2020-08-14 22:07:41 -0300

    Real datasets tests
    Description: Prepared for real datasets tests
    
    Commit: Adjust
    
    TaskNumber:#0

--- tests/test_isk2nn_streamingData.m
+++ tests/test_isk2nn_streamingData.m
@@ -24,45 +24,45 @@ lbl = 1;    % Type of labeling. 1: from sequential to [-1 and +1]
 
 %% Rotating Hyperplane
 
-% 26: 200k / 10 / 02. 
-% Moving Hyperplane. 
-% Gradual Drift.
-
-OPT =  struct('prob',26,'prob2',1,'norm',norm,'lbl',lbl);
-test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
+% % 26: 200k / 10 / 02. 
+% % Moving Hyperplane. 
+% % Gradual Drift.
+% 
+% OPT =  struct('prob',26,'prob2',1,'norm',norm,'lbl',lbl);
+% test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
 
 %% RBF Moving
 
-% 27: 200k / 10 / 05. 
-% Moving RBFs. Different Mean. 
-% Gradual drift.
-
-OPT =  struct('prob',27,'prob2',1,'norm',norm,'lbl',lbl);
-test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
+% % 27: 200k / 10 / 05. 
+% % Moving RBFs. Different Mean. 
+% % Gradual drift.
+% 
+% OPT =  struct('prob',27,'prob2',1,'norm',norm,'lbl',lbl);
+% test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
 
 %% RBF Interchange
 
-% 28: 200k / 02 / 15. 
-% Interchanging RBFs. Change Means. Abrupt drift.
-
-OPT =  struct('prob',28,'prob2',1,'norm',norm,'lbl',lbl);
-test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
+% % 28: 200k / 02 / 15. 
+% % Interchanging RBFs. Change Means. Abrupt drift.
+% 
+% OPT =  struct('prob',28,'prob2',1,'norm',norm,'lbl',lbl);
+% test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
 
 %% Moving Squares
 
-% 29: 200k / 02 / 04. 
-% Moving Squares. Gradual/Incremental drift.
-
-OPT =  struct('prob',29,'prob2',1,'norm',norm,'lbl',lbl);
-test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
+% % 29: 200k / 02 / 04. 
+% % Moving Squares. Gradual/Incremental drift.
+% 
+% OPT =  struct('prob',29,'prob2',1,'norm',norm,'lbl',lbl);
+% test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
 
 %% Transient Chessboard
 
-% 30: 200k / 02 / 08. 
-% Virtual Reocurring drifts.
-
-OPT =  struct('prob',30,'prob2',1,'norm',norm,'lbl',lbl);
-test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
+% % 30: 200k / 02 / 08. 
+% % Virtual Reocurring drifts.
+% 
+% OPT =  struct('prob',30,'prob2',1,'norm',norm,'lbl',lbl);
+% test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
 
 %% Mixed Drift
 
@@ -84,51 +84,51 @@ test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
 
 %% Weather
 
-% % 33: 18159 / 08 / 02
-% % Virtual Drift
-% 
-% OPT =  struct('prob',33,'prob2',1,'norm',norm,'lbl',lbl);
-% test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
+% 33: 18159 / 08 / 02
+% Virtual Drift
+
+OPT =  struct('prob',33,'prob2',1,'norm',norm,'lbl',lbl);
+test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
 
 %% Electricity
 
-% % 34: 45312 / 08 / 02
-% % Real Drift
-% 
-% OPT =  struct('prob',34,'prob2',1,'norm',norm,'lbl',lbl);
-% test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
+% 34: 45312 / 08 / 02
+% Real Drift
+
+OPT =  struct('prob',34,'prob2',1,'norm',norm,'lbl',lbl);
+test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
 
 %% Poker Hand
 
-% % 36: 829201 / 10 / 10
-% % Virtual Drift
-% 
-% OPT =  struct('prob',36,'prob2',1,'norm',norm,'lbl',lbl);
-% test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
+% 36: 829201 / 10 / 10
+% Virtual Drift
+
+OPT =  struct('prob',36,'prob2',1,'norm',norm,'lbl',lbl);
+test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
 
 %% Outdoor
 
-% % 37: 4000 / 21 / 40
-% % Virtual Drift
-% 
-% OPT =  struct('prob',37,'prob2',1,'norm',norm,'lbl',lbl);
-% test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
+% 37: 4000 / 21 / 40
+% Virtual Drift
+
+OPT =  struct('prob',37,'prob2',1,'norm',norm,'lbl',lbl);
+test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
 
 %% Rialto
 
-% % 38: 82250 / 27 / 10
-% % Virtual Drift
-% 
-% OPT =  struct('prob',38,'prob2',1,'norm',norm,'lbl',lbl);
-% test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
+% 38: 82250 / 27 / 10
+% Virtual Drift
+
+OPT =  struct('prob',38,'prob2',1,'norm',norm,'lbl',lbl);
+test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
 
 %% Cover Type
 
-% % 35: 581012 / 54 / 07
-% % Real Drift
-% 
-% OPT =  struct('prob',35,'prob2',1,'norm',norm,'lbl',lbl);
-% test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
+% 35: 581012 / 54 / 07
+% Real Drift
+
+OPT =  struct('prob',35,'prob2',1,'norm',norm,'lbl',lbl);
+test_isk2nn_pipeline_streaming_1data_1Ss_Nkernel(OPT);
 
 %% Spam
 

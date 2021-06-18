e842649245f1dc6e9093a14f14eaa4bbbcfad6f4
david <davidcoelho89@gmail.com>
2021-06-18 19:33:02 -0300

    DC Motor Simulink
    Description: adjusted blocks for mimo system estimation
    
    Commit: Adj
    
    TaskNumber:[#Number]

--- experiments/dcMotor_tests.m
+++ experiments/dcMotor_tests.m
@@ -53,11 +53,11 @@ figure; stem(n,u,'.');
 % Equacao do sistema!
 % y(n) = (3/4)*y(n-1) + (-1/8)*y(n-2) + (1/4)*u(n-1) + (1/4)*u(n-2);
 
-input_max_delay = 2;
+input_max_delay1 = 2;
 output_max_delay = 2;
 Ns = length(u);
 
-Ud = zeros(input_max_delay,1);
+Ud = zeros(input_max_delay1,1);
 Yd = zeros(output_max_delay,1);
 
 A = [3/4 -1/8];
@@ -79,13 +79,13 @@ figure; stem(n,y,'.');
 
 % X*v = e
 
-X = zeros(N-output_max_delay,output_max_delay+input_max_delay);
+X = zeros(N-output_max_delay,output_max_delay+input_max_delay1);
 e = zeros(N-output_max_delay,1);
 
 for i = 1:(N-output_max_delay)
     e(i) = y(i+output_max_delay);
     X(i,:) = [ flip( y( i : (i+output_max_delay-1)) ) , ...
-               flip( u( i : (i+input_max_delay-1) ) ) ];
+               flip( u( i : (i+input_max_delay1-1) ) ) ];
 end
 
 %% Estimate Coefficients
@@ -94,13 +94,13 @@ v = ((X'*X)^(-1))*X'*e;
 
 %% Predict 
 
-input_max_delay = 2;
+input_max_delay1 = 2;
 output_max_delay = 2;
 N = length(u);
 
 % Vectors with latest input and output samples
 
-Ud = zeros(input_max_delay,1);
+Ud = zeros(input_max_delay1,1);
 Yd = zeros(output_max_delay,1);
 
 % Vectors with coefficients
@@ -132,6 +132,8 @@ hold off
 
 MSE1 = (1/N)* sum((y-yh).^2);
 
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %% Init Motor Estimation
 
 clear;
@@ -148,39 +150,39 @@ sim('dcMotor_DiagramBlocks.slx',simulation_duration)
 
 % With Voltage and Load
 
-t = motor1.time';
-
-Cl1 = motor1.signals.values(:,1)';
-Ia1 = motor1.signals.values(:,2)';
-Va1 = motor1.signals.values(:,3)';
-Vel1 = motor1.signals.values(:,4)';
-
-figure;
-hold on
-plot(t,Cl1,'y')
-plot(t,Ia1,'b')
-plot(t,Va1,'r')
-plot(t,Vel1,'g')
-title('Todas as Entradas')
-hold off
+% t = motor1.time';
+% 
+% Cl1 = motor1.signals.values(:,1)';
+% Ia1 = motor1.signals.values(:,2)';
+% Va1 = motor1.signals.values(:,3)';
+% Vel1 = motor1.signals.values(:,4)';
+% 
+% figure;
+% hold on
+% plot(t,Cl1,'y')
+% plot(t,Ia1,'b')
+% plot(t,Va1,'r')
+% plot(t,Vel1,'g')
+% title('Todas as Entradas')
+% hold off
 
 % With Voltage = 0 and Load
 
-t = motor2.time';
-
-Cl2 = motor2.signals.values(:,1)';
-Ia2 = motor2.signals.values(:,2)';
-Va2 = motor2.signals.values(:,3)';
-Vel2 = motor2.signals.values(:,4)';
-
-figure;
-hold on
-plot(t,Cl2,'y')
-plot(t,Ia2,'b')
-plot(t,Va2,'r')
-plot(t,Vel2,'g')
-title('Tensão = 0')
-hold off
+% t = motor2.time';
+% 
+% Cl2 = motor2.signals.values(:,1)';
+% Ia2 = motor2.signals.values(:,2)';
+% Va2 = motor2.signals.values(:,3)';
+% Vel2 = motor2.signals.values(:,4)';
+% 
+% figure;
+% hold on
+% plot(t,Cl2,'y')
+% plot(t,Ia2,'b')
+% plot(t,Va2,'r')
+% plot(t,Vel2,'g')
+% title('Tensão = 0')
+% hold off
 
 % With Load = 0 and Voltage
 
@@ -221,12 +223,12 @@ hold off
 %% Define inputs and outputs
 
 % W / Va transfer function
-% u = Va3;
-% y = Vel3;
+u = Va3;
+y = Vel3;
 
 % % Ia / Va transfer function
-u = Va3;
-y = Ia3;
+% u = Va3;
+% y = Ia3;
 
 % % W / Cl transfer function
 % u = Cl2;
@@ -239,18 +241,18 @@ y = Ia3;
 %% Generate Prediction Vectors
 
 N = length(y);
-input_max_delay = 2;
+input_max_delay1 = 2;
 output_max_delay = 2;
 
 % X*v = e
 
-X = zeros(N-output_max_delay,output_max_delay+input_max_delay);
+X = zeros(N-output_max_delay,output_max_delay+input_max_delay1);
 e = zeros(N-output_max_delay,1);
 
 for i = 1:(N-output_max_delay)
     e(i) = y(i+output_max_delay);
     X(i,:) = [ flip( y( i : (i+output_max_delay-1)) ) , ...
-               flip( u( i : (i+input_max_delay-1) ) ) ];
+               flip( u( i : (i+input_max_delay1-1) ) ) ];
 end
 
 %% Estimate Coefficients
@@ -260,12 +262,12 @@ v = ((X'*X)^(-1))*X'*e;
 %% Predict 
 
 N = length(y);
-input_max_delay = 2;
+input_max_delay1 = 2;
 output_max_delay = 2;
 
 % Vectors with latest input and output samples
 
-Ud = zeros(input_max_delay,1);
+Ud = zeros(input_max_delay1,1);
 Yd = zeros(output_max_delay,1);
 
 % Vectors with coefficients
@@ -297,12 +299,224 @@ hold off
 
 MSE2 = (1/N)* sum((y-yh).^2);
 
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %% MISO SYSTEM - VELOCITY
 
 % W x Va, Cl
 
+clear;
+clc;
+
+%% Get Signals from Simulink
+
+% Run Simulink File
+
+simulation_duration = 10;
+sim('dcMotor_DiagramBlocks.slx',simulation_duration)
+% Tensao => Sample time: 0.001 | Noise power: 10 | Seed: [23342] |
+% Carga =>  Sample time: 0.001 | Noise power: 02 | Seed: [23341] |
+
+% With Voltage and Load
+
+t = motor1.time';
+
+Cl1 = motor1.signals.values(:,1)';
+Ia1 = motor1.signals.values(:,2)';
+Va1 = motor1.signals.values(:,3)';
+Vel1 = motor1.signals.values(:,4)';
+
+figure;
+hold on
+plot(t,Cl1,'y')
+plot(t,Ia1,'b')
+plot(t,Va1,'r')
+plot(t,Vel1,'g')
+title('Todas as Entradas')
+hold off
+
+%% Define inputs and outputs
+
+% W / Va, Cl transfer function
+u1 = Va1;
+u2 = Cl1;
+y = Vel1;
+
+%% Generate Prediction Vectors
+
+N = length(y);
+input_max_delay1 = 2;
+input_max_delay2 = 2;
+output_max_delay = 2;
+
+% X*v = e
+
+X = zeros(N-output_max_delay,output_max_delay+input_max_delay1+input_max_delay2);
+e = zeros(N-output_max_delay,1);
+
+for i = 1:(N-output_max_delay)
+    e(i) = y(i+output_max_delay);
+    X(i,:) = [ flip( y( i : (i+output_max_delay-1)) ) , ...
+               flip( u1( i : (i+input_max_delay1-1) ) ), ...
+               flip( u2( i : (i+input_max_delay2-1) ) )];
+end
+
+%% Estimate Coefficients
+
+v = ((X'*X)^(-1))*X'*e;
+
+%% Predict 
+
+N = length(y);
+input_max_delay1 = 2;
+input_max_delay2 = 2;
+output_max_delay = 2;
+
+% Vectors with latest input and output samples
+
+Ud1 = zeros(input_max_delay1,1);
+Ud2 = zeros(input_max_delay2,1);
+Yd = zeros(output_max_delay,1);
+
+% Vectors with coefficients
+
+A = v(1:output_max_delay)';
+B1 = v(output_max_delay+1:output_max_delay+input_max_delay1)';
+B2 = v(output_max_delay+input_max_delay1+1:end)';
+
+% Init predicted output
+yh = zeros(1,N);
+
+for i = 1:N
+    
+    % Predicted output (model)
+    yh(i) = A*Yd + B1*Ud1 + B2*Ud2;
+    
+    % Uptade memory
+    Ud1 = [u1(i);Ud1(1:end-1)];
+    Ud2 = [u2(i);Ud2(1:end-1)];
+    Yd = [y(i);Yd(1:end-1)];
+    
+end
+
+%% Verify Prediction
+
+figure;
+plot(t,yh,'k.');
+hold on
+plot(t,y,'r-');
+hold off
+
+MSE2 = (1/N)* sum((y-yh).^2);
+
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %% MISO SYSTEM - CURRENT
 
 % Ia x Va, Cl
+
+clear;
+clc;
+
+%% Get Signals from Simulink
+
+% Run Simulink File
+
+simulation_duration = 10;
+sim('dcMotor_DiagramBlocks.slx',simulation_duration)
+% Tensao => Sample time: 0.001 | Noise power: 10 | Seed: [23342] |
+% Carga =>  Sample time: 0.001 | Noise power: 02 | Seed: [23341] |
+
+% With Voltage and Load
+
+t = motor1.time';
+
+Cl1 = motor1.signals.values(:,1)';
+Ia1 = motor1.signals.values(:,2)';
+Va1 = motor1.signals.values(:,3)';
+Vel1 = motor1.signals.values(:,4)';
+
+figure;
+hold on
+plot(t,Cl1,'y')
+plot(t,Ia1,'b')
+plot(t,Va1,'r')
+plot(t,Vel1,'g')
+title('Todas as Entradas')
+hold off
+
+%% Define inputs and outputs
+
+% Ia / Va, Cl transfer function
+u1 = Va1;
+u2 = Cl1;
+y = Ia1;
+
+%% Generate Prediction Vectors
+
+N = length(y);
+input_max_delay1 = 2;
+input_max_delay2 = 2;
+output_max_delay = 2;
+
+% X*v = e
+
+X = zeros(N-output_max_delay,output_max_delay+input_max_delay1+input_max_delay2);
+e = zeros(N-output_max_delay,1);
+
+for i = 1:(N-output_max_delay)
+    e(i) = y(i+output_max_delay);
+    X(i,:) = [ flip( y( i : (i+output_max_delay-1)) ) , ...
+               flip( u1( i : (i+input_max_delay1-1) ) ), ...
+               flip( u2( i : (i+input_max_delay2-1) ) )];
+end
+
+%% Estimate Coefficients
+
+v = ((X'*X)^(-1))*X'*e;
+
+%% Predict 
+
+N = length(y);
+input_max_delay1 = 2;
+input_max_delay2 = 2;
+output_max_delay = 2;
+
+% Vectors with latest input and output samples
+
+Ud1 = zeros(input_max_delay1,1);
+Ud2 = zeros(input_max_delay2,1);
+Yd = zeros(output_max_delay,1);
+
+% Vectors with coefficients
+
+A = v(1:output_max_delay)';
+B1 = v(output_max_delay+1:output_max_delay+input_max_delay1)';
+B2 = v(output_max_delay+input_max_delay1+1:end)';
+
+% Init predicted output
+yh = zeros(1,N);
+
+for i = 1:N
+    
+    % Predicted output (model)
+    yh(i) = A*Yd + B1*Ud1 + B2*Ud2;
+    
+    % Uptade memory
+    Ud1 = [u1(i);Ud1(1:end-1)];
+    Ud2 = [u2(i);Ud2(1:end-1)];
+    Yd = [y(i);Yd(1:end-1)];
+    
+end
+
+%% Verify Prediction
+
+figure;
+plot(t,yh,'k.');
+hold on
+plot(t,y,'r-');
+hold off
+
+MSE2 = (1/N)* sum((y-yh).^2);
 
 %% END

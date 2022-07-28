1c62add84ea858d6314af7bc17220d432aa5de8e
david <davidcoelho89@gmail.com>
2022-07-28 09:36:17 -0300

    Spok Classifier small modifications
    Description: Spok Classifier small modifications
    
    Commit: Mod
    
    TaskNumber:#0

--- models/spokClassifier.m
+++ models/spokClassifier.m
@@ -1,11 +1,71 @@
 classdef spokClassifier
-   % HELP about spokClassifier
+   % 
+   % SParse Online adptive Kernel Classifier
+   %
+   % Properties (Hyperparameters):
+   %
+   %    number_of_epochs = <integer>
+   %        if > 1, "shows the data" more than once to the algorithm
+   %    is_stationary = [0 or 1]
+   %        
+   %    design_method = how the dictionary will be built
+   %        = 'one_dicitionary_per_class'
+   %        = 'single_dictionary'
+   %    sparsification_strategy = define how the prototypes will be selected
+   %        = 'ald'
+   %        = 'coherence'
+   %        = 'novelty'
+   %        = 'surprise'
+   %    v1 = 
+   %    v2 = 
+   %    update_strategy = define how the prototypes will be updated
+   %        = 'none'
+   %        = 'wta' (lms, unsupervised)
+   %        = 'lvq' (supervised)
+   %    update_rate = <real>
+   %        [0 to 1]
+   %    pruning_strategy = define how the prototypes will be prunned
+   %        = 'none'
+   %        = 'drift_based'
+   %        = 'hits_and_error'
+   %    min_score = 
+   %    max_prototypes = 
+   %    min_prototypes = 
+   %    video_enabled = 
+   %    nearest_neighbors = 
+   %    knn_aproximation = 
+   %    kernel_type = 
+   %    regularization = 
+   %    sigma = 
+   %    alpha = 
+   %    theta = 
+   %    gamma = 
+   %
+   % Properties (Parameters)
+   %
+   %    Cx = Clusters' centroids (prototypes)
+   %    Cy = Clusters' labels 
+   %    Km = Kernel Matrix of Entire Dictionary 
+   %    Kinv = Kernel Matrix for each class (cell) 
+   %    Kinvc = Inverse Kernel Matrix for each class (cell)
+   %    score = Used for prunning method
+   %    classification_history = Used for prunning method
+   %    times_selected = Used for prunning method
+   %    video = frame structure (can be played with 'video function')
+   %    Yh = Hold all predictions
+   %
+   % Methods:
+   %
+   %	spokClassifier()        % Constructor
+   %	partial_fit(self,x,y)	% Training Function (1 instance)
+   %	fit(self,X,Y)           % Training Function (N instances)
+   %	Yh = predict(self,X)    % Prediction Function
    
    % Hyperparameters
    properties
-        number_of_epochs = 5;
+        number_of_epochs = 1;
         is_stationary = 0;
-        design_method = 'per_class';
+        design_method = 'one_dicitionary_per_class';
         sparsification_strategy = 'ald';
         v1 = 0.1;
         v2 = 0.9;
@@ -42,7 +102,7 @@ classdef spokClassifier
    end
    
    methods
-       
+
        % Constructor
        function self = spokClassifier()
            % Set the hyperparameters after initializing!
@@ -56,6 +116,40 @@ classdef spokClassifier
        
        % Training Function (N instances)
        function self = fit(self,X,Y)
+           
+           for ep = 1:self.number_of_epochs
+               
+               % Problem Initialization
+               [Nc,N] = size(Y);        % Total of classes and samples
+               self.Yh = -1*ones(Nc,N); % Initialize outputs
+               
+               % Shuffle Data
+               if(self.is_stationary)
+                   I = randperm(N);
+                   X = X(:,I);
+                   Y = Y(:,I);
+               end
+               
+               for n = 1:N
+                   
+                   xn = X(:,n);
+                   yn = Y(:,n);
+                   
+                   [~,number_of_prototypes] = size(self.Cx);
+                   
+                   if(number_of_prototypes == 0)
+                       self.Yh(1,n) = 1; % Make a guess (yh=1: first class)
+                       self = self.dictionaryGrow(self,xn,yn);
+                       continue; % call next sample
+                   end
+                   
+                   
+                   
+               end
+               
+               
+           end
+           
            % ToDo - All
            self = self + X + Y;
        end
@@ -75,16 +169,27 @@ classdef spokClassifier
            % ToDo - All
            self = self + x + y;
        end
-           
        
-   end    
+       function self = dictionaryPrune(self,x,y)
+           % ToDo - All
+           self = self + x + y;
+       end
+       
+       function self = addSample(self,x,y)
+           % ToDo - All
+           self = self + x + y;
+       end
+       
+       function self = removeSample(self,x,y)
+           % ToDo - All
+           self = self + x + y;
+       end
+       
+       function self = updateScore(self,x,y)
+           % ToDo - All
+           self = self + x + y;
+       end
+       
+   end
     
 end
-
-
-
-
-
-
-
-

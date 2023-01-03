de9413ec4de8d93d66b3b7239b89da7768f65cea
david <davidcoelho89@gmail.com>
2023-01-03 12:36:15 -0300

    rlsArx model
    Description: finished rlsArx model
    
    Commit: add
    
    TaskNumber:#0

--- classification/rls_train.m
+++ classification/rls_train.m
@@ -18,15 +18,15 @@ function [PARout] = rls_train(DATA,PAR)
 
 %% SET DEFAULT HYPERPARAMETERS
 
-if ((nargin == 1) || (isempty(PAR))),
+if ((nargin == 1) || (isempty(PAR)))
     PARaux.lambda = 1;      % dont have forgiving factor
     PARaux.Von = 0;         % disable video 
     PAR = PARaux;
 else
-    if (~(isfield(PAR,'lambda'))),
+    if (~(isfield(PAR,'lambda')))
         PAR.lambda = 1;
     end
-    if (~(isfield(PAR,'Von'))),
+    if (~(isfield(PAR,'Von')))
         PAR.Von = 0;
     end
 end    
@@ -46,14 +46,14 @@ Von = PAR.Von;              % enable or disable video
 [No,N] = size(Y);           % Number of outputs and samples
 
 % Weight Matrix Init
-if (isfield(PAR,'W')), 
+if (isfield(PAR,'W'))
     W = PAR.W;              % if already initialized
 else
     W = zeros(p+1,No);   	% W0; could be W = 0.01*rand(p,No);
 end
 
 % P Matrix Init
-if (isfield(PAR,'P')), 
+if (isfield(PAR,'P'))
     P = PAR.P;              % if already initialized
 else
     P = 1e+4 * eye(p+1);    % P0;
@@ -67,7 +67,7 @@ VID = struct('cdata',cell(1,N),'colormap', cell(1,N));
 
 %% ALGORITHM
 
-for i = 1:N,
+for i = 1:N
     
     % Get sample (input and output)
     x = X(:,i);
@@ -80,7 +80,7 @@ for i = 1:N,
     P = (1/lambda)*(P - K*x'*P);
     
     % Save frame of the current epoch
-    if (Von),
+    if (Von)
         VID(i) = hyperplane_frame(W',DATA);
     end
     

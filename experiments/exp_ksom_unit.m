748715dadc230defc62fb27335941fcdf1814279
david <davidcoelho89@gmail.com>
2023-02-10 14:57:42 -0300

    finished main loop of exp_ksom_unit

--- experiments/exp_ksom_unit.m
+++ experiments/exp_ksom_unit.m
@@ -39,7 +39,6 @@ MP.lambda = 2.0;    % Jpbc = Ds + lambda * Err
 
 %% CHOOSE FIXED HYPERPARAMETERS 
 
-SOMp_acc = cell(OPT.Nr,1);	% Init of Acc Hyperparameters of SOM-2D
 HP_som.lbl = prot_lbl;      % Neurons' labeling function
 HP_som.ep = 200;            % max number of epochs
 HP_som.k = [5 4];           % number of neurons (prototypes)
@@ -57,7 +56,6 @@ HP_som.K = 1;            	% Number of nearest neighbors (classify)
 HP_som.knn_type = 2;        % Type of knn aproximation
 HP_som.Ktype = 0;           % Non-kernelized Algorithm
 
-KSOMGDp_acc = cell(OPT.Nr,1); % Init of Acc Hyperparameters of KSOM-GD1
 HP_ksomgd.lbl = prot_lbl;    % Neurons' labeling function
 HP_ksomgd.ep = 200;          % max number of epochs
 HP_ksomgd.k = [5 4];         % number of neurons (prototypes)
@@ -76,7 +74,6 @@ HP_ksomgd.knn_type = 2;      % Type of knn aproximation
 HP_ksomgd.Ktype = 1;         % Type of Kernel
 HP_ksomgd.sigma = 0.5;   	 % Variance (gaussian, log, cauchy kernel)
 
-KSOMEFp_acc = cell(OPT.Nr,1); % Init of Acc Hyperparameters of KSOM-EF1
 HP_ksomef.lbl = prot_lbl;	 % Neurons' labeling function
 HP_ksomef.ep = 200;          % max number of epochs
 HP_ksomef.k = [5 4];         % number of neurons (prototypes)
@@ -121,28 +118,32 @@ DATA = label_encode(DATA,OPT);      % adjust labels for the problem
 
 %% ACCUMULATORS AND HANDLERS
 
-hold_acc = cell(OPT.Nr,1);          % Acc of labels and data division
+data_acc = cell(OPT.Nr,1);            % Acc of labels and data division
+NAMES = {'som','ksomgd','ksomef'};    % Acc of names for plots
 
-som_out_tr = cell(OPT.Nr,1);        % Acc of training data output
-som_out_ts = cell(OPT.Nr,1);        % Acc of test data output
-som_stats_tr_acc = cell(OPT.Nr,1);  % 
-som_stats_ts_acc = cell(OPT.Nr,1);  % 
+nstats_all_tr = cell(length(NAMES),1);
+nstats_all_ts = cell(length(NAMES),1);
 
-som_Mconf_sum = zeros(Nc,Nc);       % Aux var for mean confusion matrix calc
+SOMp_acc = cell(OPT.Nr,1);	          % Acc Parameters of SOM
+som_out_tr = cell(OPT.Nr,1);          % Acc of training data output
+som_out_ts = cell(OPT.Nr,1);          % Acc of test data output
+som_stats_tr_acc = cell(OPT.Nr,1);    % Acc of training statistics
+som_stats_ts_acc = cell(OPT.Nr,1);    % Acc of test statistics
+som_Mconf_sum = zeros(Nc,Nc);         % Aux var for mean conf mat calc
 
-ksomgd_out_tr = cell(OPT.Nr,1);	    % Acc of training data output
-ksomgd_out_ts = cell(OPT.Nr,1);	    % Acc of test data output
-ksomgd_stats_tr_acc = cell(OPT.Nr,1); % 
-ksomgd_stats_ts_acc = cell(OPT.Nr,1); %
+KSOMGDp_acc = cell(OPT.Nr,1);         % Acc Parameters of KSOM-GD
+ksomgd_out_tr = cell(OPT.Nr,1);	      % Acc of training data output
+ksomgd_out_ts = cell(OPT.Nr,1);	      % Acc of test data output
+ksomgd_stats_tr_acc = cell(OPT.Nr,1); % Acc of training statistics
+ksomgd_stats_ts_acc = cell(OPT.Nr,1); % Acc of test statistics
+ksomgd_Mconf_sum = zeros(Nc,Nc);      % Aux var for mean conf mat calc
 
-ksomgd_Mconf_sum = zeros(Nc,Nc);    % Aux var for mean confusion matrix calc
-
-ksomef_out_tr = cell(OPT.Nr,1);	    % Acc of training data output
-ksomef_out_ts = cell(OPT.Nr,1);	    % Acc of test data output
-ksomef_stats_tr_acc = cell(OPT.Nr,1); % 
-ksomef_stats_ts_acc = cell(OPT.Nr,1); %
-
-ksomef_Mconf_sum = zeros(Nc,Nc);    % Aux var for mean confusion matrix calc
+KSOMEFp_acc = cell(OPT.Nr,1);           % Acc Parameters of KSOM-EF
+ksomef_out_tr = cell(OPT.Nr,1);	        % Acc of training data output
+ksomef_out_ts = cell(OPT.Nr,1);	        % Acc of test data output
+ksomef_stats_tr_acc = cell(OPT.Nr,1);   % Acc of training statistics
+ksomef_stats_ts_acc = cell(OPT.Nr,1);   % Acc of test statistics
+ksomef_Mconf_sum = zeros(Nc,Nc);        % Aux var for mean conf mat calc
 
 %% FILE NAME
 
@@ -162,7 +163,7 @@ display(datestr(now));
 
 [DATAho] = hold_out(DATA,OPT);
 
-hold_acc{r} = DATAho;
+data_acc{r} = DATAho;
 DATAtr = DATAho.DATAtr;
 DATAts = DATAho.DATAts;
 
@@ -223,7 +224,7 @@ KSOMEFp_acc{r} = ksom_ef_train(DATAtr,HP_ksomef);
 som_out_tr{r} = som_classify(DATAtr,SOMp_acc{r});
 som_stats_tr_acc{r} = class_stats_1turn(DATAtr,som_out_tr{r});
 
-som_out_ts{r,1} = som_classify(DATAts,SOMp_acc{r});
+som_out_ts{r} = som_classify(DATAts,SOMp_acc{r});
 som_stats_ts_acc{r} = class_stats_1turn(DATAts,som_out_ts{r});
 
 % KSOM-GD
@@ -248,15 +249,40 @@ end
 
 % Statistics for n turns
 
+som_nstats_tr = class_stats_nturns(som_stats_tr_acc);
+som_nstats_ts = class_stats_nturns(som_stats_ts_acc);
+
+ksomgd_nstats_tr = class_stats_nturns(ksomgd_stats_tr_acc);
+ksomgd_nstats_ts = class_stats_nturns(ksomgd_stats_ts_acc);
+
+ksomef_nstats_tr = class_stats_nturns(ksomef_stats_tr_acc);
+ksomef_nstats_ts = class_stats_nturns(ksomef_stats_ts_acc);
+
+% Get all Statistics in one Cell
+
+nstats_all_tr{1,1} = som_nstats_tr;
+nstats_all_tr{2,1} = ksomgd_nstats_tr;
+nstats_all_tr{3,1} = ksomef_nstats_tr;
+
+nstats_all_ts{1,1} = som_nstats_ts;
+nstats_all_ts{2,1} = ksomgd_nstats_ts;
+nstats_all_ts{3,1} = ksomef_nstats_ts;
+
+% Compare Training and Test Statistics
+
+class_stats_ncomp(nstats_all_tr,NAMES);
+
+class_stats_ncomp(nstats_all_ts,NAMES);
+
 % Mean Confusion Matrix
 
-som_Mconf_sum = som_Mconf_sum / OPT.Nr;
-ksomgd_Mconf_sum = ksomgd_Mconf_sum / OPT.Nr;
-ksomef_Mconf_sum = ksomef_Mconf_sum / OPT.Nr;
-
-som_Mconf_sum2 = [som_Mconf_sum(1,1) sum(som_Mconf_sum(1,2:end)) ; sum(som_Mconf_sum(2:end,1)) sum(sum(som_Mconf_sum(2:end,2:end)))];
-ksomgd_Mconf_sum2 = [ksomgd_Mconf_sum(1,1) sum(ksomgd_Mconf_sum(1,2:end)) ; sum(ksomgd_Mconf_sum(2:end,1)) sum(sum(ksomgd_Mconf_sum(2:end,2:end)))];
-ksomef_Mconf_sum2 = [ksomef_Mconf_sum(1,1) sum(ksomef_Mconf_sum(1,2:end)) ; sum(ksomef_Mconf_sum(2:end,1)) sum(sum(ksomef_Mconf_sum(2:end,2:end)))];
+% som_Mconf_sum = som_Mconf_sum / OPT.Nr;
+% ksomgd_Mconf_sum = ksomgd_Mconf_sum / OPT.Nr;
+% ksomef_Mconf_sum = ksomef_Mconf_sum / OPT.Nr;
+% 
+% som_Mconf_sum2 = [som_Mconf_sum(1,1) sum(som_Mconf_sum(1,2:end)) ; sum(som_Mconf_sum(2:end,1)) sum(sum(som_Mconf_sum(2:end,2:end)))];
+% ksomgd_Mconf_sum2 = [ksomgd_Mconf_sum(1,1) sum(ksomgd_Mconf_sum(1,2:end)) ; sum(ksomgd_Mconf_sum(2:end,1)) sum(sum(ksomgd_Mconf_sum(2:end,2:end)))];
+% ksomef_Mconf_sum2 = [ksomef_Mconf_sum(1,1) sum(ksomef_Mconf_sum(1,2:end)) ; sum(ksomef_Mconf_sum(2:end,1)) sum(sum(ksomef_Mconf_sum(2:end,2:end)))];
 
 % Calculate "Binary" Statistics
 % - Define which classes will be in class "1" and "2"
@@ -266,100 +292,100 @@ ksomef_Mconf_sum2 = [ksomef_Mconf_sum(1,1) sum(ksomef_Mconf_sum(1,2:end)) ; sum(
 
 % Init labels' cells and Init boxplot matrix
 
-labels = {};
-
-Mat_boxplot1 = []; % Train Multiclass
-Mat_boxplot2 = []; % Train Binary
-Mat_boxplot3 = []; % Test Multiclass
-Mat_boxplot4 = []; % Test Binary
-
-% SOM-2D
-
-[~,n_labels] = size(labels);
-n_labels = n_labels+1;
-labels(1,n_labels) = {'SOM 2D'};
-Mat_boxplot1 = [Mat_boxplot1 accuracy_mult(som_out_tr)];
-Mat_boxplot2 = [Mat_boxplot2 accuracy_bin(som_out_tr)];
-Mat_boxplot3 = [Mat_boxplot3 accuracy_mult(som_out_ts)];
-Mat_boxplot4 = [Mat_boxplot4 accuracy_bin(som_out_ts)];
-
-% KSOM-GD
-
-[~,n_labels] = size(labels);
-n_labels = n_labels+1;
-labels(1,n_labels) = {'KSOM-GD'};
-Mat_boxplot1 = [Mat_boxplot1 accuracy_mult(ksomgd_out_tr)];
-Mat_boxplot2 = [Mat_boxplot2 accuracy_bin(ksomgd_out_tr)];
-Mat_boxplot3 = [Mat_boxplot3 accuracy_mult(ksomgd_out_ts)];
-Mat_boxplot4 = [Mat_boxplot4 accuracy_bin(ksomgd_out_ts)];
-
-% KSOM-EF
-
-[~,n_labels] = size(labels);
-n_labels = n_labels+1;
-labels(1,n_labels) = {'KSOM-EF'};
-Mat_boxplot1 = [Mat_boxplot1 accuracy_mult(ksomef_out_tr)];
-Mat_boxplot2 = [Mat_boxplot2 accuracy_bin(ksomef_out_tr)];
-Mat_boxplot3 = [Mat_boxplot3 accuracy_mult(ksomef_out_ts)];
-Mat_boxplot4 = [Mat_boxplot4 accuracy_bin(ksomef_out_ts)];
+% labels = {};
+% 
+% Mat_boxplot1 = []; % Train Multiclass
+% Mat_boxplot2 = []; % Train Binary
+% Mat_boxplot3 = []; % Test Multiclass
+% Mat_boxplot4 = []; % Test Binary
+% 
+% % SOM-2D
+% 
+% [~,n_labels] = size(labels);
+% n_labels = n_labels+1;
+% labels(1,n_labels) = {'SOM 2D'};
+% Mat_boxplot1 = [Mat_boxplot1 accuracy_mult(som_out_tr)];
+% Mat_boxplot2 = [Mat_boxplot2 accuracy_bin(som_out_tr)];
+% Mat_boxplot3 = [Mat_boxplot3 accuracy_mult(som_out_ts)];
+% Mat_boxplot4 = [Mat_boxplot4 accuracy_bin(som_out_ts)];
+% 
+% % KSOM-GD
+% 
+% [~,n_labels] = size(labels);
+% n_labels = n_labels+1;
+% labels(1,n_labels) = {'KSOM-GD'};
+% Mat_boxplot1 = [Mat_boxplot1 accuracy_mult(ksomgd_out_tr)];
+% Mat_boxplot2 = [Mat_boxplot2 accuracy_bin(ksomgd_out_tr)];
+% Mat_boxplot3 = [Mat_boxplot3 accuracy_mult(ksomgd_out_ts)];
+% Mat_boxplot4 = [Mat_boxplot4 accuracy_bin(ksomgd_out_ts)];
+% 
+% % KSOM-EF
+% 
+% [~,n_labels] = size(labels);
+% n_labels = n_labels+1;
+% labels(1,n_labels) = {'KSOM-EF'};
+% Mat_boxplot1 = [Mat_boxplot1 accuracy_mult(ksomef_out_tr)];
+% Mat_boxplot2 = [Mat_boxplot2 accuracy_bin(ksomef_out_tr)];
+% Mat_boxplot3 = [Mat_boxplot3 accuracy_mult(ksomef_out_ts)];
+% Mat_boxplot4 = [Mat_boxplot4 accuracy_bin(ksomef_out_ts)];
 
 %% GRAPHICS - DISPLAY
 
-% BOXPLOT 1
-figure; boxplot(Mat_boxplot1, 'label', labels);
-set(gcf,'color',[1 1 1])        % Tira o fundo Cinza do Matlab
-ylabel('Accuracy')              % label eixo y
-xlabel('Classifiers')           % label eixo x
-title('Classifiers Comparison') % Titulo
-axis ([0 n_labels+1 0 1.05])	% Eixos
-
-hold on
-media1 = mean(Mat_boxplot1);    % Taxa de acerto média
-max1 = max(Mat_boxplot1);       % Taxa máxima de acerto
-plot(media1,'*k')
-hold off
-
-% BOXPLOT 2
-figure; boxplot(Mat_boxplot2, 'label', labels);
-set(gcf,'color',[1 1 1])        % Tira o fundo Cinza do Matlab
-ylabel('Accuracy')              % label eixo y
-xlabel('Classifiers')           % label eixo x
-title('Classifiers Comparison') % Titulo
-axis ([0 n_labels+1 0 1.05])	% Eixos
-
-hold on
-media2 = mean(Mat_boxplot2);    % Taxa de acerto média
-max2 = max(Mat_boxplot2);       % Taxa máxima de acerto
-plot(media2,'*k')
-hold off
-
-% BOXPLOT 3
-figure; boxplot(Mat_boxplot3, 'label', labels);
-set(gcf,'color',[1 1 1])        % Tira o fundo Cinza do Matlab
-ylabel('Accuracy')              % label eixo y
-xlabel('Classifiers')           % label eixo x
-title('Classifiers Comparison') % Titulo
-axis ([0 n_labels+1 0 1.05])	% Eixos
-
-hold on
-media3 = mean(Mat_boxplot3);    % Taxa de acerto média
-max3 = max(Mat_boxplot3);       % Taxa máxima de acerto
-plot(media3,'*k')
-hold off
-
-% BOXPLOT 4
-figure; boxplot(Mat_boxplot4, 'label', labels);
-set(gcf,'color',[1 1 1])        % Tira o fundo Cinza do Matlab
-ylabel('Accuracy')              % label eixo y
-xlabel('Classifiers')           % label eixo x
-title('Classifiers Comparison') % Titulo
-axis ([0 n_labels+1 0 1.05])	% Eixos
-
-hold on
-media4 = mean(Mat_boxplot4);    % Taxa de acerto média
-max4 = max(Mat_boxplot4);       % Taxa máxima de acerto
-plot(media4,'*k')
-hold off
+% % BOXPLOT 1
+% figure; boxplot(Mat_boxplot1, 'label', labels);
+% set(gcf,'color',[1 1 1])        % Tira o fundo Cinza do Matlab
+% ylabel('Accuracy')              % label eixo y
+% xlabel('Classifiers')           % label eixo x
+% title('Classifiers Comparison') % Titulo
+% axis ([0 n_labels+1 0 1.05])	% Eixos
+% 
+% hold on
+% media1 = mean(Mat_boxplot1);    % Taxa de acerto média
+% max1 = max(Mat_boxplot1);       % Taxa máxima de acerto
+% plot(media1,'*k')
+% hold off
+% 
+% % BOXPLOT 2
+% figure; boxplot(Mat_boxplot2, 'label', labels);
+% set(gcf,'color',[1 1 1])        % Tira o fundo Cinza do Matlab
+% ylabel('Accuracy')              % label eixo y
+% xlabel('Classifiers')           % label eixo x
+% title('Classifiers Comparison') % Titulo
+% axis ([0 n_labels+1 0 1.05])	% Eixos
+% 
+% hold on
+% media2 = mean(Mat_boxplot2);    % Taxa de acerto média
+% max2 = max(Mat_boxplot2);       % Taxa máxima de acerto
+% plot(media2,'*k')
+% hold off
+% 
+% % BOXPLOT 3
+% figure; boxplot(Mat_boxplot3, 'label', labels);
+% set(gcf,'color',[1 1 1])        % Tira o fundo Cinza do Matlab
+% ylabel('Accuracy')              % label eixo y
+% xlabel('Classifiers')           % label eixo x
+% title('Classifiers Comparison') % Titulo
+% axis ([0 n_labels+1 0 1.05])	% Eixos
+% 
+% hold on
+% media3 = mean(Mat_boxplot3);    % Taxa de acerto média
+% max3 = max(Mat_boxplot3);       % Taxa máxima de acerto
+% plot(media3,'*k')
+% hold off
+% 
+% % BOXPLOT 4
+% figure; boxplot(Mat_boxplot4, 'label', labels);
+% set(gcf,'color',[1 1 1])        % Tira o fundo Cinza do Matlab
+% ylabel('Accuracy')              % label eixo y
+% xlabel('Classifiers')           % label eixo x
+% title('Classifiers Comparison') % Titulo
+% axis ([0 n_labels+1 0 1.05])	% Eixos
+% 
+% hold on
+% media4 = mean(Mat_boxplot4);    % Taxa de acerto média
+% max4 = max(Mat_boxplot4);       % Taxa máxima de acerto
+% plot(media4,'*k')
+% hold off
 
 %% BEST AND WORST MCONF
 

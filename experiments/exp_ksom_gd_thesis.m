0f5bdd4771527b0771330e563b613e9c9ba5049b
david <davidcoelho89@gmail.com>
2023-03-11 15:07:34 -0300

    finished exp with motFail 1 and ksom

--- experiments/exp_ksom_gd_thesis.m
+++ experiments/exp_ksom_gd_thesis.m
@@ -17,16 +17,16 @@ format long e;  % Output data style (float)
 OPT.Nr = 05;        % Number of repetitions of each algorithm
 OPT.alg = 'ksomgd'; % Which Classifier will be used
 OPT.prob = 07;      % Which problem will be solved / used
-OPT.prob2 = 02;     % When it needs an specification of data set
+OPT.prob2 = 01;     % When it needs an specification of data set
 OPT.norm = 3;       % Normalization definition
 OPT.lbl = 1;        % Data labeling definition
-OPT.hold = 02;      % Hold out method
+OPT.hold = 01;      % Hold out method
 OPT.ptrn = 0.7;     % Percentage of samples for training
-OPT.hpo = 'random'; % 'grid' ; 'random' ; 'none'
+OPT.hpo = 'none'; % 'grid' ; 'random' ; 'none'
 
 OPT.savefile = 1;   % decides if file will be saved
 
-OPT.calculate_bin = 1;  % [0 or 1] decides to calculate binary statistics
+OPT.calculate_bin = 0;  % [0 or 1] decides to calculate binary statistics
 OPT.class_1_vect = 1;   % [2,3] which classes belongs together
 
 % Prototypes' labeling definition

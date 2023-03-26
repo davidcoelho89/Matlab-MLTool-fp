074f92e70b95481c6a80b3e24dfec73a9574aaf6
david <davidcoelho89@gmail.com>
2023-03-26 17:38:28 -0300

    began clustering sample

---
+++ samples/cluster_unit_test_oop.m
@@ -0,0 +1,52 @@
+%% MACHINE LEARNING TOOLBOX
+
+% Clustering Algorithms - Unit Test
+% Author: David Nascimento Coelho
+% Last Update: 2023/03/20
+
+close;          % Close all windows
+clear;          % Clear all variables
+clc;            % Clear command window
+format long e;  % Output data style (float)
+
+%% CHOOSE EXPERIMENT PARAMETERS
+
+
+
+%% CHOOSE FIXED HYPERPARAMETERS 
+
+
+
+%% CHOOSE HYPERPARAMETERS TO BE OPTIMIZED
+
+
+
+%% ACCUMULATORS
+
+
+
+%% HANDLERS FOR CLASSIFICATION FUNCTIONS
+
+
+
+%% DATA LOADING, PRE-PROCESSING, VISUALIZATION
+
+
+
+%% HOLD OUT / NORMALIZE / SHUFFLE / HPO / TRAINING / TEST / STATISTICS
+
+
+
+%% RESULTS / STATISTICS
+
+
+
+%% GRAPHICS - OF LAST TURN
+
+
+
+%% SAVE VARIABLES AND VIDEO
+
+
+
+%% END

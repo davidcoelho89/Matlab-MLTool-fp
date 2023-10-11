b0be28690a14883c527b42686ea15113efc79309
david <davidcoelho89@gmail.com>
2023-10-11 10:16:09 -0300

    removed scripts of matlab learning

--- experiments/exp_script.m
+++ experiments/exp_script.m
@@ -790,4 +790,36 @@ format long e;
 % plot(x,y,'b-')
 % axis([-N/2 N/2 -N/2 N/2])
 
+%% DISTANCIAS (normas)
+
+% pdist2();               % distância quadrática
+% norm();                 % varias distâncias (normas)
+
+%% ORGANIZAR DADOS
+
+% sort();    	% Organiza em ordem crescente ou decrescente
+
+% bubble sort   % Organizacao por borbulhamento
+                % Colocar criterio de parada
+
+% matc = importdata('Derma.m');
+% %vetor auxiliar
+% vetaux = zeros(1,35);
+% cont2 = 0;
+% cont3 = 0;
+% for i = 1:365,
+%     for j = 1:365,
+%         if (matc(j,35) > matc(j+1,35)),
+%             vetaux = matc(j,:);
+%             matc(j,:) = matc(j+1,:);
+%             matc(j+1,:) = vetaux;
+%         end
+%     end
+% end
+
+%% FUNÇÕES GERAIS
+
+X = [1 1 -1 -1 1 1 1]'; % inicializa vetor de rotulos
+tabulate(X);            % porcentagem de cada rotulo
+
 %% END

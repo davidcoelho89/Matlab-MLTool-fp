754041e381cb1c4bdde217684e5338cc2bbe5e26
david <davidcoelho89@gmail.com>
2024-01-19 09:40:35 -0300

    started spark result analysis

---
+++ results_analysis/spark-motorFailure/spark_motorFailure_result_analysis.m
@@ -0,0 +1,17 @@
+% %% RESULT ANALYSIS
+
+% SPARK and Stationary Data Sets
+% Author: David Nascimento Coelho
+% Last Update: 2024/01/12
+
+close;          % Close all windows
+clear;          % Clear all variables
+clc;            % Clear command window
+
+format long e;  % Output data style (float)
+
+%%
+
+
+
+%% END

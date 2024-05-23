240c5486fbf173961a758f63943803b6f93afeed
david <davidcoelho89@gmail.com>
2024-05-23 15:05:25 -0300

    deleted results of spark and motorFailure 1

---
+++ results_analysis/spark-motorFailure-02/change_filenames.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Change filename in order to put it in a new pattern
+"""
+
+import os
+
+old_file_1 = "motorFailure_isk2nn_hpo1_norm3_Dm"
+old_file_2 = ["1","2"]
+old_file_3 = "_Ss"
+old_file_4 = ["1","2","3","4"]
+old_file_5 = "_Us0_Ps0_"
+old_file_6 = ["cau","exp","gau","kmod","lin","log","pol","sig"]
+old_file_7 = "_"
+old_file_8 = ["1","2"]
+old_file_9 = "nn.mat"
+
+# motorFailure_2_ksomef_hold_1_hpo_1_norm_3_lbl_1_nn_1_Nep_50_Nprot_30_Kt_1.mat
+
+new_file_1 = "motorFailure_2_spok_hold_2_norm_3_hpo_1_Dm"
+
+for Dm in old_file_2:
+    for Ss in old_file_4:
+        for Kt in old_file_6:
+            for NN in old_file_8:
+                old_file_name = old_file_1 + Dm + old_file_3 + Ss + \
+                                old_file_5 + Kt + old_file_7 + NN + \
+                                old_file_9
+                print(old_file_name)
+                new_file_name = new_file_1 + Dm + old_file_3 + Ss + \
+                                old_file_5 + Kt + old_file_7 + NN + \
+                                old_file_9
+                print(new_file_name)
+
+
+"""
+old_file_name = "motorFailure_isk2nn_hpo1_norm3_Dm1_Ss1_Us0_Ps0_cau_1nn.mat"
+new_file_name = "motorFailure_spark_hpo1_norm3_Dm1_Ss1_Us0_Ps0_cau_1nn.mat"
+
+try:
+    os.rename(old_file_name, new_file_name)
+except FileNotFoundError:
+    print(f"{old_file_name} does not exist.")
+"""  
+  

67ba94f4470ea23e9ae5c608225d093a1146ba77
david <davidcoelho89@gmail.com>
2024-05-29 20:45:56 -0300

    finished tests ksomgd wallfollow hold 1

--- experiments/exp_ksom_data.m
+++ experiments/exp_ksom_data.m
@@ -47,13 +47,13 @@ MP.gamma = 0.1;     % Jpbc = Ds + lambda * Err + gamma * mcc
 % 1: linear | 2: rbf | 3: polynomial | 4: exp | 
 % 5: cauchy | 6: log | 7: sigmoid | 8: kmod |
 
-kernels = 6;
-% kernels = [1,2,3,4,5,6,7,8];
+% kernels = 6;
+kernels = [1,2,3,4,5,7,8];
 
 % Which Prototype Labeling Strategy to use
 
 % prot_lbl = [1,2,3];
-prot_lbl = 2;
+prot_lbl = [1,3];
 
 % Hyperparameters - Default
 

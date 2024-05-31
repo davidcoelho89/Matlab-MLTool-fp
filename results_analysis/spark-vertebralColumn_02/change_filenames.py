0eba4f31b72fb1331b706b9cab40c83c3eb8eca6
david <davidcoelho89@gmail.com>
2024-05-31 10:21:42 -0300

    changed names of spark vertColumn results 3

--- results_analysis/spark-vertebralColumn_02/change_filenames.py
+++ results_analysis/spark-vertebralColumn_02/change_filenames.py
@@ -5,11 +5,11 @@ Change filename in order to put it in a new pattern
 
 import os
 
-old_file_1 = "vertebral1_spok_hpo1_norm3_Dm"
+old_file_1 = "vertebral2_spok_hpo1_norm3_Dm"
 old_file_2 = ["1","2"]
 old_file_3 = "_Ss"
 # old_file_4 = ["1","2","3","4"]
-old_file_4 = ["3","4"]
+old_file_4 = ["1","2"]
 old_file_5 = "_Us0_Ps0_"
 old_file_6 = ["cau","exp","gau","kmod","lin","log","pol","sig"]
 old_file_7 = "_nn"
@@ -18,7 +18,7 @@ old_file_9 = ".mat"
 
 # vertebral_1_spok_hpo1_norm3_Dm
 
-new_file_1 = "vertebral_1_spok_hpo1_norm3_Dm"
+new_file_1 = "vertebral_2_spok_hpo1_norm3_Dm"
 
 for Dm in old_file_2:
     for Ss in old_file_4:

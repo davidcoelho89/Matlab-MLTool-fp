e44c5a54af63835a189208261fa60535e7f5645f
david <davidcoelho89@gmail.com>
2024-06-07 10:47:52 -0300

    began spark iris result analysis

---
+++ results_analysis/spark_iris_result_analysis.m
@@ -0,0 +1,11 @@
+%% RESULT ANALYSIS
+
+% SPARK and Stationary Data Sets
+% Author: David Nascimento Coelho
+% Last Update: 2024/01/30
+
+close;          % Close all windows
+clear;          % Clear all variables
+clc;            % Clear command window
+
+format long e;  % Output data style (float)
